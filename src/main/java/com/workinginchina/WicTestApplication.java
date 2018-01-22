fa1a1487e89f66322681a8d8b0c6a3fa936b4517
Juan Pablo Oquendo <jpo_54@hotmail.com>
2018-01-22 08:01:40 +0800

    first commit

---
+++ src/main/java/com/workinginchina/WicTestApplication.java
@@ -0,0 +1,48 @@
+package com.workinginchina;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
+@RestController
+public class WicTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WicTestApplication.class, args);
+	}
+	
+	@RequestMapping("/something")
+	public String getSomething() {
+		try {
+			Thread.sleep(10*1000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		return "Some message";
+	}
+}
+
+@RestController
+class ServiceInstanceRestController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+}
